a0e04a79fabe9682b9dfffe25d19c1aba3afabb7
rubyist <scott.barron@github.com>
2014-09-23 11:21:46 -0400

    Extract some git commands, start cleaning up push

---
+++ hash_object_test.go
@@ -0,0 +1,19 @@
+package pktline
+
+import (
+	"bytes"
+	"github.com/bmizerany/assert"
+	"io"
+	"testing"
+)
+
+func TestHashObject(t *testing.T) {
+	reader := bytes.NewBufferString("hello, world\n")
+
+	gitHash, _ := NewHashObject()
+	io.Copy(gitHash, reader)
+	gitHash.Close()
+
+	hash := gitHash.Hash()
+	assert.Equal(t, "4b5fa63702dd96796042e92787f464e28f09f17d", hash)
+}
